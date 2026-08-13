32d79a63bfa2c22bbb7b702a1d141cd5f334255a
sapon <sapon@Taskia.lan>
2026-08-13 08:53:26 -0700

    Save

---
+++ SeleniumGit/src/classwork/HelloWorlf.java
@@ -0,0 +1,9 @@
+package classwork;
+
+public class HelloWorlf {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+	}
+
+}
---
+++ SeleniumGit/src/classwork/JavaTest.java
@@ -0,0 +1,10 @@
+package classwork;
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		System.out.println("Java is a popular programming language.");
+
+	}
+
+}
