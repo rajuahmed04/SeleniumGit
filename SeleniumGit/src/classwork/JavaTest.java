346b74b49bc2359ddaacc5eb37617f95b67c373f
sapon <sapon@Taskia.lan>
2026-08-14 08:34:51 -0700

    Add Codes

--- SeleniumGit/src/classwork/JavaTest.java
+++ SeleniumGit/src/classwork/JavaTest.java
@@ -5,6 +5,7 @@ public class JavaTest {
 	public static void main(String[] args) {
 		System.out.println("Java is a popular programming language.");
 		System.out.println("Java works on different Operating System");
+		System.out.println("Java is used for creating any apps.");
 
 	}
 
