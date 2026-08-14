ab2a8dc93527751305199820114caac2740b32b8
rajuahmed04 <88064942+rajuahmed04@users.noreply.github.com>
2026-08-14 08:15:23 -0700

    Update JavaTest.java
    Add more codes

--- SeleniumGit/src/classwork/JavaTest.java
+++ SeleniumGit/src/classwork/JavaTest.java
@@ -4,6 +4,7 @@ public class JavaTest {
 
 	public static void main(String[] args) {
 		System.out.println("Java is a popular programming language.");
+		System.out.println("Java works on different Operating System");
 
 	}
 
