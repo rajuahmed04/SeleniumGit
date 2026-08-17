1bf9447c903cb5fbe1a74c7deba51af66fb09ffc
Your Name <rahmansm44@gmail.com>
2026-08-17 16:15:02 -0700

    Save from Bash

--- SeleniumGit/src/classwork/Animal.java
+++ SeleniumGit/src/classwork/Animal.java
@@ -8,24 +8,31 @@ class Animal {
 
 	class Pig extends Animal {
 	  public void animalSound() {
-	    System.out.println("The pig says: wee wee");
+	    System.out.println("The pig makes sound: wee wee");
 	  }
 	}
 
 	class Dog extends Animal {
 	  public void animalSound() {
-	    System.out.println("The dog says: bow wow");
+	    System.out.println("The dog makes sound: bow wow");
 	  }
 	}
+     class Cat extends Animal{
+      public void animalSound(){
+       System.out.println("The Cat makes sound: meow meow");
+        }
+       }
 
 	class Main {
 	  public static void main(String[] args) {
 	    Animal myAnimal = new Animal();
 	    Animal myPig = new Pig();
 	    Animal myDog = new Dog();
+            Animal myCat = new Cat();
 	        
 	    myAnimal.animalSound();
 	    myPig.animalSound();
 	    myDog.animalSound();
+            myCat.animalSound();
 	  }
 	}
