c8003deddf33f44fc86bc7b34e95e0ca2eaf3eb0
Your Name <rahmansm44@gmail.com>
2026-08-17 16:31:36 -0700

    Fix from Bash

--- SeleniumGit/src/classwork/HelloWorld.java
+++ SeleniumGit/src/classwork/HelloWorld.java
@@ -5,12 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	System.out.println("Hello World");
 	System.out.println("The word is like an egg shape.");
-<<<<<<< HEAD
         System.out.println("The word is moving like on Eclipse");
-
-=======
         System.out.println("The word is moving around the World");
->>>>>>> af2686f33fc1dba7f0b9267f063f4393d34b08c5
+
 	}
 
 }
